cb72cb74772b00c3a933e088509310abfbf7ac98
IvanNik85 <ivannikolic0885@gmail.com>
2019-11-15 22:00:31 +0100

    responsive news menu

--- src/Components/News/News.js
+++ src/Components/News/News.js
@@ -6,7 +6,7 @@ export default class News extends React.Component {
         click: false       
     }
     changeValue = (q) => {
-        this.setState({ click: q })
+        window.innerWidth > 1140 && this.setState({ click: q })
     }
     rotateCircle = () => {      
         const rotate = {transform: `rotate(${(this.state.click * -45)}deg)`}
@@ -17,7 +17,7 @@ export default class News extends React.Component {
         element.classList.remove('added');
         setTimeout(() => {
             element.classList.add('added'); 
-        },0)         
+        },50)         
     }
 
     render() {       
@@ -25,45 +25,51 @@ export default class News extends React.Component {
             <div className="container-fluid" id="News">
                 <h1>News</h1>
                 <div className="row">
-                    <div className="col-md-8 content">
-                        <div className="row news">
-
-                            <div className="col-md-4 image"></div>
-                            <div className="col-md-8 text">
-                                <h5>News title</h5>
-                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam ducimus repudiandae perspiciatis assumenda deleniti vitae laudantium aliquid sed molestias totam molestiae, nulla expedita facere, incidunt unde nisi officia atque? Omnis laborum excepturi facere eos quisquam cupiditate provident, cum maiores aliquam nam. Sequi, sunt? Nemo maxime reprehenderit dignissimos saepe voluptate similique quae a dolor. Harum suscipit placeat veniam sunt exercitationem ex eligendi vero accusantium sapiente sint quis distinctio aliquam facilis ducimus earum quisquam dolore cumque inventore eum, perferendis sed! Alias error dicta fugiat sapiente totam? Quo dignissimos reprehenderit facilis voluptates est ut maxime? Itaque possimus ea eos odio mollitia ipsum doloremque!</p>
+                    <div className="col-md-9 col-lg-8 content">
+                        <div className="row">
+                            <div className="navMenu"></div>
+                        </div>
+                        <div className="row">
+                            <div className="col-12 col-md-10 col-xl-8 news">
+                                <div className="image"></div>
+                                <div className="text">
+                                    <h5>News title</h5>
+                                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam ducimus repudiandae perspiciatis assumenda deleniti vitae laudantium aliquid sed molestias totam molestiae, nulla expedita facere, incidunt unde nisi officia atque? Omnis laborum excepturi facere eos quisquam cupiditate provident, cum maiores aliquam nam. Sequi, sunt? Nemo maxime reprehenderit dignissimos saepe voluptate similique quae a dolor. Harum suscipit placeat veniam sunt exercitationem ex eligendi vero accusantium sapiente sint quis distinctio aliquam facilis ducimus earum quisquam dolore cumque inventore eum, perferendis sed! Alias error dicta fugiat sapiente totam? Quo dignissimos reprehenderit facilis voluptates est ut maxime? Itaque possimus ea eos odio mollitia ipsum doloremque!</p>
+                                    <a href="##" className="readMore">Read more<i className="fas fa-angle-double-right"></i></a>
+                                </div>
                             </div>
-
                         </div>
-                        <div className="row news">
-
-                            <div className="col-md-4 image"></div>
-                            <div className="col-md-8 text">
-                                <h5>News title</h5>
-                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam ducimus repudiandae perspiciatis assumenda deleniti vitae laudantium aliquid sed molestias totam molestiae, nulla expedita facere, incidunt unde nisi officia atque? Omnis laborum excepturi facere eos quisquam cupiditate provident, cum maiores aliquam nam. Sequi, sunt? Nemo maxime reprehenderit dignissimos saepe voluptate similique quae a dolor. Harum suscipit placeat veniam sunt exercitationem ex eligendi vero accusantium sapiente sint quis distinctio aliquam facilis ducimus earum quisquam dolore cumque inventore eum, perferendis sed! Alias error dicta fugiat sapiente totam? Quo dignissimos reprehenderit facilis voluptates est ut maxime? Itaque possimus ea eos odio mollitia ipsum doloremque!</p>
+                        <div className="row">
+                            <div className="col-12 col-md-10 col-xl-8 news">
+                                <div className="image"></div>
+                                <div className="text">
+                                    <h5>News title</h5>
+                                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam ducimus repudiandae perspiciatis assumenda deleniti vitae laudantium aliquid sed molestias totam molestiae, nulla expedita facere, incidunt unde nisi officia atque? Omnis laborum excepturi facere eos quisquam cupiditate provident, cum maiores aliquam nam. Sequi, sunt? Nemo maxime reprehenderit dignissimos saepe voluptate similique quae a dolor. Harum suscipit placeat veniam sunt exercitationem ex eligendi vero accusantium sapiente sint quis distinctio aliquam facilis ducimus earum quisquam dolore cumque inventore eum, perferendis sed! Alias error dicta fugiat sapiente totam? Quo dignissimos reprehenderit facilis voluptates est ut maxime? Itaque possimus ea eos odio mollitia ipsum doloremque!</p>
+                                    <a href="##" className="readMore">Read more<i className="fas fa-angle-double-right"></i></a>
+                                </div>
                             </div>
-
                         </div>
-                        <div className="row news">
-
-                            <div className="col-md-4 image"></div>
-                            <div className="col-md-8 text">
-                                <h5>News title</h5>
-                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam ducimus repudiandae perspiciatis assumenda deleniti vitae laudantium aliquid sed molestias totam molestiae, nulla expedita facere, incidunt unde nisi officia atque? Omnis laborum excepturi facere eos quisquam cupiditate provident, cum maiores aliquam nam. Sequi, sunt? Nemo maxime reprehenderit dignissimos saepe voluptate similique quae a dolor. Harum suscipit placeat veniam sunt exercitationem ex eligendi vero accusantium sapiente sint quis distinctio aliquam facilis ducimus earum quisquam dolore cumque inventore eum, perferendis sed! Alias error dicta fugiat sapiente totam? Quo dignissimos reprehenderit facilis voluptates est ut maxime? Itaque possimus ea eos odio mollitia ipsum doloremque!</p>
+                        <div className="row">
+                            <div className="col-12 col-md-10 col-xl-8 news">                                
+                                <div className="image"></div>
+                                <div className="text">
+                                    <h5>News title</h5>
+                                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam ducimus repudiandae perspiciatis assumenda deleniti vitae laudantium aliquid sed molestias totam molestiae, nulla expedita facere, incidunt unde nisi officia atque? Omnis laborum excepturi facere eos quisquam cupiditate provident, cum maiores aliquam nam. Sequi, sunt? Nemo maxime reprehenderit dignissimos saepe voluptate similique quae a dolor. Harum suscipit placeat veniam sunt exercitationem ex eligendi vero accusantium sapiente sint quis distinctio aliquam facilis ducimus earum quisquam dolore cumque inventore eum, perferendis sed! Alias error dicta fugiat sapiente totam? Quo dignissimos reprehenderit facilis voluptates est ut maxime? Itaque possimus ea eos odio mollitia ipsum doloremque!</p>  
+                                    <a href="##" className="readMore">Read more<i className="fas fa-angle-double-right"></i></a>
+                                </div>
+                                <div className="more">
+                                    <a href="##">More News <i className="fas fa-angle-double-right"></i></a>
+                                </div>
                             </div>
-
-                        </div>
-                        <div className="more">
-                            <a href="##">More News <i className="fas fa-angle-double-right"></i></a>
-                        </div>
+                        </div>                        
                     </div>
-                    <div className="col-md-4 circleDiv">
+                    <div className="col-md-3 col-lg-4 circleDiv">
                        <div className="outsideDiv">
                             <div className="sideDiv" style={this.state.click ? { transform: `rotate(${(this.state.click * 45)}deg)` } : null}>
                                 <div className="circle">
                                     <span
                                         onClick={() => {
-                                            this.changeValue(1);
+                                            this.changeValue(-3);
                                             this.downUpMotion();
                                             }
                                         }
@@ -74,26 +80,36 @@ export default class News extends React.Component {
                                 <div className="circle">
                                     <span
                                         onClick={() => {
-                                            this.changeValue(-1)
+                                            this.changeValue(3)
                                             this.downUpMotion();
                                             } 
                                         }
-                                        style={this.state.click ? { transform: `rotate(${(this.state.click * (-45))}deg)` }: null}>
+                                        style={this.state.click ? this.rotateCircle() : null}>
                                         Actual
                                     </span>
                                 </div>
                                 <div className="circle">
                                     <span
                                         onClick={() => {
-                                            this.changeValue(3);
+                                            this.changeValue(-1);
                                             this.downUpMotion();
                                             }
                                         } 
-                                        style={this.state.click ? { transform: `rotate(${(this.state.click * (-45))}deg)` }: null}>
+                                        style={this.state.click ? this.rotateCircle() : null}>
                                         Older
                                     </span>
                                 </div>
-                                <div className="circle"><span></span></div>                                
+                                <div className="circle">
+                                    <span
+                                        onClick={() => {
+                                            this.changeValue(1);
+                                            this.downUpMotion();
+                                            }
+                                        } 
+                                        style={this.state.click ? this.rotateCircle() : null}>
+                                        Popular
+                                    </span>
+                                </div>                                
                             </div>                               
                             <div className="activeNews"></div>                 
                        </div>   
