ebc4e876be70beba2844ad496741c2e51a799ee7
IvanNik85 <ivannikolic0885@gmail.com>
2020-01-09 22:55:26 +0100

    added animated phone images

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,68 +1,81 @@
 import React from 'react'
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 import "./Home.scss"
 import "../Button/Button.scss"
 import MainClass from "../MainClass/MainClass"
 import Info from "../Info/Info"
 import Contact from "../Contact/Contact"
 import News from "../News/News"
+import images from "../../Images"
 // import NewsMap from "../News/NewsMap"
 
-export default class Home extends React.Component {    
+export default class Home extends React.Component {
     state = {
-        visible: false    
-    }    
-    myref = React.createRef();   
+        visible: false,
+        im: []
+    }
+    myref = React.createRef();
     options = () => {
-        if(window.innerWidth > 768) {
-            return {rootMargin: "-300px 0px"}
-        } else {            
-            return {rootMargin: "-150px 0px"}
-        }  
-    } 
+        if (window.innerWidth > 768) {
+            return { rootMargin: "-300px 0px" }
+        } else {
+            return { rootMargin: "-150px 0px" }
+        }
+    }
     // options = {
     //     rootMargin: "-300px"
     // }
     observer = new IntersectionObserver(entries => {
-        if(entries[0].intersectionRatio > 0) {
-            this.setState({visible: true }); 
+        if (entries[0].intersectionRatio > 0) {
+            this.setState({ visible: true });
         } else {
-            this.setState({visible: false }); 
+            this.setState({ visible: false });
         }
-    }, this.options())         
-    
-    componentDidMount() {       
-        this.observer.observe(this.myref.current)       
-    }     
-    
-    render () {    
-        const styleIt = {animation: 'show 2s ease forwards'}        
+    }, this.options())
+   
+    componentDidMount() {
+        this.observer.observe(this.myref.current)   
+        this.imagesMap()     
+    }    
+
+    imagesMap() {
+        const sources = images.map(img => img.src)
+        this.setState({im: sources})
+    }
+
+    render() {
+        const styleIt = { animation: 'show 2s ease forwards' }
+        console.log(this.state.im)
         return (
             <div>
-                <MainClass />  
-                <Info />  
-                <div className="container-fluid Home" id="Home" ref={this.myref}>  
+                <MainClass />
+                <Info />
+                <div className="container-fluid Home" id="Home" ref={this.myref}>
                     <div className="row">
                         {/* <img src={mainImage} className="img-fluid mainImg" alt="mainImage"/> */}
+                        <div className="animImages">
+                            <div className="rec firstImg"><img src={this.state.im[0]} alt="phoneOne" /></div>
+                            <div className="rec secondImg"><img src={this.state.im[1]} alt="phoneTwo" /></div>
+                            <div className="rec thirdImg"><img src={this.state.im[2]} alt="phoneThree" /></div>
+                        </div>
                         <div className="mainButtons" style={this.state.visible ? styleIt : null}>
                             <div className="pulse">
                                 <Link to="/ProductList">
                                     Products
                                     <div className="backPulse"></div>
-                                </Link>                    
+                                </Link>
                             </div>
                             <div className="pulse">
                                 <Link to="/Cart">
                                     Cart
                                     <div className="backPulse"></div>
-                                </Link>                    
+                                </Link>
                             </div>
-                        </div>        
-                          
-                    </div>    
-                </div>   
-                <News />               
-                <Contact />      
+                        </div>
+                    </div>
+                </div>
+                <News />
+                <Contact />
             </div>
         )
     }
---
+++ src/Images.js
@@ -0,0 +1,7 @@
+const images = [
+    { id: 1, src: 'images/huawei-honor-20-lite.jpg', title: 'phoneOne'},
+    { id: 2, src: 'images/samsung-galaxy-s10+.jpg', title: 'phoneTwo'},
+    { id: 3, src: 'images/LG_V40_ThinQ.jpg', title: 'phoneThree'},
+]
+
+export default images;  
